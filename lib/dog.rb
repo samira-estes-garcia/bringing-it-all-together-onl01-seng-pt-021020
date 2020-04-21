ac0ec2520e799effe2ae3c1ac0ae2eceb458a21c
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-21 03:50:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,7 +63,7 @@ class Dog
   end
   
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ? LIMIT 1", name, breed)
     
     if !dog.empty?
       dog_data = dog[0]
